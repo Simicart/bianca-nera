9c9571963fead0d977f0c6996e20296595404bcc
Robert <mrthinlt@gmail.com>
2020-03-24 10:56:46 +0700

    Fix sticker menu

--- reactjs-pwa/packages/siminia/src/simi/App/Bianca/Header/Component/HeaderNavigation.js
+++ reactjs-pwa/packages/siminia/src/simi/App/Bianca/Header/Component/HeaderNavigation.js
@@ -69,7 +69,6 @@ class Navigation extends React.Component{
         let id = e.currentTarget.id || '';
         $(`.app-nav .${id} .sub-item`).css({marginTop: $(e.currentTarget).outerHeight()});
         $(`.app-nav .${id} .sub-item`).addClass('active')
-
     }
     hoverDisableItem = (e) => {
         let id = e.currentTarget.id || '';
