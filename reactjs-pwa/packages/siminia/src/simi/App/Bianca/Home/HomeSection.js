ee1d983aa69228933f78d23420392598cc42ddc3
Robert <mrthinlt@gmail.com>
2020-06-10 16:42:24 +0700

    Optimize home page

--- reactjs-pwa/packages/siminia/src/simi/App/Bianca/Home/HomeSection.js
+++ reactjs-pwa/packages/siminia/src/simi/App/Bianca/Home/HomeSection.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import Identify from "src/simi/Helper/Identify";
-import productBySkus from 'src/simi/queries/catalog/getProductsBySkus.graphql';
+import productBySkus from 'src/simi/queries/catalog/getProductsBySkusHome.graphql';
 import { Simiquery } from 'src/simi/Network/Query';
 import {productUrlSuffix, cateUrlSuffix} from 'src/simi/Helper/Url';
 import Griditem from './Homesection/Griditem';
@@ -19,6 +19,7 @@ const HomeSection = props => {
     const {homesections} = data && data.home && data.home.homesections || [];
     const {storeConfig} = Identify.getStoreConfig() || {};
     const base_url = storeConfig && storeConfig.base_url || window.location.href || '';
+    const [productsData, setProductsData] = useState(null);
 
     const [activeItemIndex, setActiveItemIndex] = useState(0);
 
@@ -78,110 +79,128 @@ const HomeSection = props => {
         setActiveItemIndex(newActiveValue);
     }
 
-    const renderProducts = (skus, index) => {
-        if (skus.length) {
-            let variables = {
-                stringSku: skus,
-                pageSize: 3,
-                currentPage: 1
-            }
-            return <Simiquery query={productBySkus} variables={variables}>
-                {({ loading, error, data }) => {
-                    if (error) return null;
-                    //if (error) return <div>{Identify.__('Data Fetch Error')}</div>;
-                    //prepare data
-                    if (data && data.simiproducts) {
-                        data.products = applySimiProductListItemExtraField(data.simiproducts)
-                        if (data.products.simi_filters)
-                            data.products.filters = data.products.simi_filters
-                            
-                        const stringVar = JSON.stringify({...variables, ...{currentPage: 0}})
-                        if (!loadedData || !loadedData.vars || loadedData.vars !== stringVar) {
-                            loadedData = data
-                        } else {
-                            let loadedItems = loadedData.products.items
-                            const newItems = data.products.items
-                            loadedItems = loadedItems.concat(newItems)
-                            for(var i=0; i<loadedItems.length; ++i) {
-                                for(var j=i+1; j<loadedItems.length; ++j) {
-                                    if(loadedItems[i] && loadedItems[j] && loadedItems[i].id === loadedItems[j].id)
-                                        loadedItems.splice(j--, 1);
-                                }
-                            }
-                            loadedData.products.items = loadedItems
+    const ItemRender = (props) => {
+        const {item} = props
+        let price = '';
+        const currency = item.price.regularPrice.amount.currency || '$';
+        const regular = item.price.regularPrice.amount.value || '$';
+        if (item && item.special_price) {
+            price = <div className="product-prices">
+                <div className="regular">
+                    <span className="price">{currency === '$' ? `${currency}${item.special_price}` : `${currency} ${item.special_price}`}</span>
+                </div>
+                <div className="special">
+                    <span class="price old">{currency === '$' ? `${currency}${regular}` : `${currency} ${regular}`}</span>
+                    {/* <span class="sale_off">-20%</span> */}
+                </div>
+            </div>
+        } else {
+            price = <div className="product-prices">
+                <div className="regular">
+                    <span className="price">{currency === '$' ? `${currency}${regular}` : `${currency} ${regular}`}</span>
+                </div>
+            </div>
+        }
+        return <div className="siminia-product-grid-item">
+                <div className="grid-item-image" style={{position: "relative"}}>
+                    <div className="siminia-product-image" style={{backgroundColor: "white"}}>
+                        <div style={{position: "absolute", left: "0px", top: "0px", bottom: "0px", width: "100%"}}>
+                            <a href={`${item.url_key}.html`} title={item.sku}>
+                                <img src={item.thumbnail.url} alt={item.thumbnail.label} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="siminia-product-des">
+                    <div className="product-des-info">
+                        <div className="product-name">
+                            <div className="product-name small">{item.name}</div>
+                        </div>
+                        <div className="vendor-and-price">
+                            <div className="prices-layout " id="price-2204">
+                                <div className="price-configurable">
+                                    {price}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    }
+
+    const renderProducts = (skus) => {
+        if (skus && skus.length) {
+            let productItems = productsData && productsData.items && productsData.items.filter((item) => skus.includes(item.sku)) || []
+            if (isPhone && productsData && productItems.length) {
+                    let carouselIndex = activeItemIndex[index] || 0;
+                    if (Identify.isRtl()) {
+                        if (carouselIndex === 0) carouselIndex = (productItems.length - 1);
+                        productItems.items.reverse();
+                    }
+                    return <ItemsCarousel
+                        infiniteLoop={false}
+                        gutter={13} //Space between cards.
+                        firstAndLastGutter={false}
+                        activePosition={'center'}
+                        disableSwipe={false}
+                        alwaysShowChevrons={false}
+                        numberOfCards={2}
+                        slidesToScroll={1}
+                        outsideChevron={true}
+                        showSlither={false}
+                        activeItemIndex={carouselIndex}
+                        requestToChangeActive={(num) => carouselOnChange(num, index)}
+                        chevronWidth={16}
+                        leftChevron={<ChevronLeft className="chevron-left" style={{width: `16px`, height: `16px`}} />}
+                        rightChevron={<ChevronRight className="chevron-right" style={{width: `16px`, height: `16px`}} />}
+                        classes={{ wrapper: "wrapper", itemsWrapper: 'items-wrapper', itemsInnerWrapper: 'items-inner-wrapper', itemWrapper: 'item-wrapper', rightChevronWrapper: 'right-chevron-wrapper', leftChevronWrapper: 'left-chevron-wrapper' }}
+                    >
+                        {productItems.map((item, index) => {
+                                return <ItemRender
+                                    item={item}
+                                    handleLink={onClickProduct}
+                                    lazyImage={true}
+                                    key={index} />
+                            })
                         }
-                        loadedData.vars = stringVar
-                    }
-                    if (loadedData && loadedData.category && parseInt(loadedData.category.id) === parseInt(id)){
-                        data = loadedData
-                    }
-                
-                    if (!data || !data.simiproducts) {
-                        return <Loading />
-                    }
-                    if (data.products.items.length === 0){
-                        return null;
-                    }
+                    </ItemsCarousel>
+                }
 
-                    //let products = data && data.products || null;
-                    let products = data && data.simiproducts || null;
-
-                    if (isPhone && products && products.items.length
-                    ) {
-                        let carouselIndex = activeItemIndex[index] || 0;
-                        if (Identify.isRtl()) {
-                            if (carouselIndex === 0) carouselIndex = (products.items.length - 1);
-                            products.items.reverse();
-                        }
-                        return <ItemsCarousel
-                            infiniteLoop={false}
-                            gutter={13} //Space between cards.
-                            firstAndLastGutter={false}
-                            activePosition={'center'}
-                            disableSwipe={false}
-                            alwaysShowChevrons={false}
-                            numberOfCards={2}
-                            slidesToScroll={1}
-                            outsideChevron={true}
-                            showSlither={false}
-                            activeItemIndex={carouselIndex}
-                            requestToChangeActive={(num) => carouselOnChange(num, index)}
-                            chevronWidth={16}
-                            leftChevron={<ChevronLeft className="chevron-left" style={{width: `16px`, height: `16px`}} />}
-                            rightChevron={<ChevronRight className="chevron-right" style={{width: `16px`, height: `16px`}} />}
-                            classes={{ wrapper: "wrapper", itemsWrapper: 'items-wrapper', itemsInnerWrapper: 'items-inner-wrapper', itemWrapper: 'item-wrapper', rightChevronWrapper: 'right-chevron-wrapper', leftChevronWrapper: 'left-chevron-wrapper' }}
-                        >
-                            {products.items.map((item, index) => {
-                                    return <Griditem
-                                        item={item}
-                                        handleLink={onClickProduct}
-                                        lazyImage={true}
-                                        key={index} />
-                                })
-                            }
-                        </ItemsCarousel>
-                    }
-
-                    return (products && products.items.map((item, index) => {
-                            return <Griditem
-                                item={item}
-                                handleLink={onClickProduct}
-                                lazyImage={true}
-                                key={index} />
-                        })
-                    );
-                }}
-            </Simiquery>
+                return (productItems && productItems.map((item, index) => {
+                        return <ItemRender
+                            item={item}
+                            handleLink={onClickProduct}
+                            lazyImage={true}
+                            key={index} />
+                    })
+                );
         }
         return '';
     }
 
     const renderSection = () => {
+        let skus = {};
+        homesections.map((item, index) => {
+            if (!skus[index]) skus[index] = [];
+            if (item.product_id_1 && parseInt(item.product_id_1) !== 0) skus[index].push(item.product_id_1);
+            if (item.product_id_2 && parseInt(item.product_id_2) !== 0) skus[index].push(item.product_id_2);
+            if (item.product_id_3 && parseInt(item.product_id_3) !== 0) skus[index].push(item.product_id_3);
+            return true;
+        })
+        let allSkus = [];
+        for (let i in skus) {
+            skus[i].length && skus[i].forEach(sku => {
+                allSkus.push(sku);
+            });
+        }
+
+        const variables = {stringSkus: allSkus} /** All sku to query one time */
+
         return homesections.map((item, index) => {
-            let skus = [];
-            if (item.product_id_1 && parseInt(item.product_id_1) !== 0) skus.push(item.product_id_1);
-            if (item.product_id_2 && parseInt(item.product_id_2) !== 0) skus.push(item.product_id_2);
-            if (item.product_id_3 && parseInt(item.product_id_3) !== 0) skus.push(item.product_id_3);
+            let sectionSkus = [];
+            if (item.product_id_1 && parseInt(item.product_id_1) !== 0) sectionSkus.push(item.product_id_1);
+            if (item.product_id_2 && parseInt(item.product_id_2) !== 0) sectionSkus.push(item.product_id_2);
+            if (item.product_id_3 && parseInt(item.product_id_3) !== 0) sectionSkus.push(item.product_id_3);
 
             const imgLeft1 = isPhone && item.image_left_1_mobile ? item.image_left_1_mobile : item.image_left_1;
             const imgLeft1_alt = imgLeft1 && imgLeft1.split('/').pop().split('.')[0].replace(/[_-]/g, ' ');
@@ -190,7 +209,7 @@ const HomeSection = props => {
 
             return (
                 <div className="homesection" key={index}>
-                    <div className={`left-sec ${skus.length === 0 ? 'no-products':''}`}>
+                    <div className={`left-sec ${sectionSkus.length === 0 ? 'no-products':''}`}>
                         {item.image_left_1 && 
                             <div className="image-1" onClick={() => onClickImage(item, '1')}>
                                 <img src={imgLeft1} alt={imgLeft1_alt}/>
@@ -202,9 +221,33 @@ const HomeSection = props => {
                             </div>
                         }
                     </div>
-                    {skus.length > 0 &&
+                    {allSkus.length > 0 && sectionSkus.length > 0 &&
                         <div className="right-sec">
-                            {renderProducts(skus, index)}
+                            {index === 0 && productsData === null &&
+                                <Simiquery query={productBySkus} variables={variables}>
+                                    {({ loading, error, data }) => {
+                                        if (error) return null;
+
+                                        if (!data || !data.products) {
+                                            return <Loading />
+                                        }
+                                        if (data.products.items.length === 0){
+                                            return null;
+                                        }
+                                        let products = data && data.products || null;
+                                        if (products) {
+                                            setProductsData(products);
+                                        }
+                                        return null;
+                                    }}
+                                </Simiquery>
+                            }
+                            {index !== 0 && productsData === null &&
+                                <Loading />
+                            }
+                            {productsData &&
+                                renderProducts(sectionSkus)
+                            }
                         </div>
                     }
                 </div>
