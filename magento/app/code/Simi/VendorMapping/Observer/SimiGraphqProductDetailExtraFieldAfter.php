2536ffdc40d2ff2b4bec00638c0e615e2dc1b5a8
Robert <mrthinlt@gmail.com>
2020-07-01 09:03:11 +0700

    Optimize get gc timezone by product type id

--- magento/app/code/Simi/VendorMapping/Observer/SimiGraphqProductDetailExtraFieldAfter.php
+++ magento/app/code/Simi/VendorMapping/Observer/SimiGraphqProductDetailExtraFieldAfter.php
@@ -32,8 +32,13 @@ class SimiGraphqProductDetailExtraFieldAfter implements ObserverInterface {
     public function execute(\Magento\Framework\Event\Observer $observer) {
         // add giftcard product timezone options
         $object = $observer->getObject();
-        $object->extraFields['aw_gc_timezones'] = $this->getTimezones();
-        $object->extraFields['aw_gc_template_image_url_path'] = $this->mediaConfig->getTmpMediaUrl('');
+        if (isset($object->extraFields['attribute_values']['type_id']) && $object->extraFields['attribute_values']['type_id'] == 'aw_giftcard') {
+            $object->extraFields['aw_gc_timezones'] = $this->getTimezones();
+            $object->extraFields['aw_gc_template_image_url_path'] = $this->mediaConfig->getTmpMediaUrl('');
+        } else {
+            $object->extraFields['aw_gc_timezones'] = [];
+            $object->extraFields['aw_gc_template_image_url_path'] = '';
+        }
         return $this;
     }
 
