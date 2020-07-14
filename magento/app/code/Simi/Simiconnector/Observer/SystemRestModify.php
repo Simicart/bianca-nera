05d2accc2de8dd32beaafb49f289e9f8a94485b4
Cody Nguyen <cody@simicart.com>
2020-07-14 16:47:48 +0700

    Observer moving

--- magento/app/code/Simi/Simiconnector/Observer/SystemRestModify.php
+++ magento/app/code/Simi/Simiconnector/Observer/SystemRestModify.php
@@ -38,14 +38,18 @@ class SystemRestModify implements ObserverInterface
 		       }
                if (isset($contentArray['totals']['items'])) {
                    $totalData = $contentArray['totals'];
-                   $this->_addDataToQuoteItem($totalData);
+                   $this->_addDataToQuoteItem($totalData, true);
                    $contentArray['totals'] = $totalData;
                }
 	       } else if (
 		       strpos($routeData['routePath'], 'V1/guest-carts/:cartId') !== false ||
 		       strpos($routeData['routePath'], 'V1/carts/mine') !== false
 	       ) {
-		       $this->_addDataToQuoteItem($contentArray);
+                if (strpos($routeData['routePath'], 'V1/carts/mine/totals') !== false) {
+    		       $this->_addDataToQuoteItem($contentArray, true);
+                } else {
+                   $this->_addDataToQuoteItem($contentArray, false);
+                }
 	       } else if (strpos($routeData['routePath'], 'integration/customer/token') !== false) {
 		       $this->_addCustomerIdentity($contentArray, $requestContent, $request);
 	       }
@@ -69,7 +73,9 @@ class SystemRestModify implements ObserverInterface
     }
 
     //modify quote item
-    private function _addDataToQuoteItem(&$contentArray) {
+    private function _addDataToQuoteItem(&$contentArray, $isTotal) {
+        // if ($isTotal)
+        //     return;
         if (isset($contentArray['items']) && is_array($contentArray['items'])) {
             foreach ($contentArray['items'] as $index => $item) {
                 $quoteItem = $this->simiObjectManager
@@ -78,6 +84,7 @@ class SystemRestModify implements ObserverInterface
                     $product = $this->simiObjectManager
                         ->create('Magento\Catalog\Model\Product')
                         ->load($quoteItem->getData('product_id'));
+
                     $item['simi_image']  = $this->simiObjectManager
                         ->create('Simi\Simiconnector\Helper\Products')
                         ->getImageProduct($product);
@@ -101,8 +108,94 @@ class SystemRestModify implements ObserverInterface
                         ->create('Simi\Simiconnector\Helper\Products')
                         ->getImageProduct($imageProductModel);
 
-
                     $contentArray['items'][$index] = $item;
+
+
+                    $product = $this->simiObjectManager
+                        ->create('Magento\Catalog\Model\Product')
+                        ->load($quoteItem->getData('product_id'));
+                    //if(!$isTotal)
+                        $contentArray['items'][$index]['attribute_values'] = $product->toArray();
+                    $contentArray['items'][$index]['is_buy_service'] = $quoteItem->getData('is_buy_service');
+                    //add giftcard info to quote item
+                    if ($product->getTypeId() == 'aw_giftcard'){
+                        $quoteItemCollection = $this->simiObjectManager->get('Magento\Quote\Model\Quote\Item\Option')->getCollection();
+                        $quoteItemCollection->addFieldToFilter('item_id', $quoteItem->getId());
+                        $requestData = [];
+                        foreach ($quoteItemCollection as $option) {
+                            $requestData[$option->getCode()] = $option->getValue();
+                        }
+                        $contentArray['items'][$index]['giftcard_values'] = $requestData;
+                    }
+                }
+                //modify option values for special products (preorder, trytobuy)
+                if ($isTotal && isset($item['options']) && is_string($item['options']) && $optionArray = json_decode($item['options'], true)) {
+                    if (is_array($optionArray)) {
+                        $systemProductOption = array();
+                        $newOptions = false;
+                        $extraFieldIndex = false;
+                        foreach ($optionArray as $itemOption) {
+                            if ($itemOption['label'] === \Simi\Simicustomize\Model\Api\Quoteitems::TRY_TO_BUY_OPTION_TITLE) {
+                                $extraFieldIndex = 'simi_trytobuy_option';
+                            } else if ($itemOption['label'] === \Simi\Simicustomize\Model\Api\Quoteitems::PRE_ORDER_OPTION_TITLE) {
+                                $extraFieldIndex = 'simi_pre_order_option';
+                            }
+                            if ($extraFieldIndex) {
+                                $systemProductOption = json_decode(base64_decode($itemOption['full_view']), true);
+                                $newOptions = $systemProductOption;
+                                foreach ($newOptions as $newOptionIndex => $newOption ) {
+                                    $newOptions[$newOptionIndex] = array(
+                                        'label' => $newOption['sku'],
+                                        'value' => $newOption['quantity'],
+                                        'full_view' => $newOption['name']
+                                    );
+                                    $productModel = $this->simiObjectManager->create(\Magento\Catalog\Model\Product::class);
+                                    $productModel->load($productModel->getIdBySku($newOption['sku']));
+                                    $systemProductOption[$newOptionIndex]['product_final_price'] = $productModel->getFinalPrice();
+                                    $systemProductOption[$newOptionIndex]['name'] = $productModel->getName();
+                                    $systemProductOption[$newOptionIndex]['url_key'] = $productModel->getData('url_key');
+                                    $systemProductOption[$newOptionIndex]['vendor_id'] = $productModel->getData('vendor_id');
+
+                                    //to get image + parent data
+                                    $product = $this->simiObjectManager
+                                        ->create('Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable')
+                                        ->getParentIdsByChild($productModel->getId());
+                                    $imageProductModel = $productModel;
+                                    if($product && isset($product[0])){
+                                        $media_gallery = $imageProductModel->getMediaGallery();
+                                        $parentProductModel = $this->simiObjectManager->create(\Magento\Catalog\Model\Product::class)->load($product[0]);
+                                        if ($media_gallery && isset($media_gallery['images']) && is_array($media_gallery['images']) && !count($media_gallery['images'])) {
+                                            $imageProductModel = $parentProductModel;
+                                        }
+                                        $systemProductOption[$newOptionIndex]['name'] = $parentProductModel->getName();
+                                        $systemProductOption[$newOptionIndex]['url_key'] = $parentProductModel->getData('url_key');
+                                        $systemProductOption[$newOptionIndex]['vendor_id'] = $productModel->getData('vendor_id');
+                                    }
+                                    $systemProductOption[$newOptionIndex]['image'] =  $this->simiObjectManager
+                                        ->create('Simi\Simiconnector\Helper\Products')
+                                        ->getImageProduct($imageProductModel);
+
+                                    if (isset($newOption['request']['super_attribute']) && is_array($newOption['request']['super_attribute'])) {
+                                        $frontendOption = [];
+                                        foreach ($newOption['request']['super_attribute'] as $attributeid=>$attribute) {
+                                            $eavModel = $this->simiObjectManager->get('Magento\Catalog\Model\ResourceModel\Eav\Attribute')->load($attributeid);
+                                            $frontendOption[] = array(
+                                                'label'=>$eavModel->getFrontendLabel(),
+                                                'value' => $eavModel->getFrontend()->getValue($productModel)
+                                            );
+                                        }
+
+                                        $systemProductOption[$newOptionIndex]['frontend_option'] = $frontendOption;
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        $contentArray['items'][$index][$extraFieldIndex] = json_encode($systemProductOption);
+
+                        if ($newOptions)
+                            $contentArray['items'][$index]['options'] = json_encode($newOptions);
+                    }
                 }
             }
         }
