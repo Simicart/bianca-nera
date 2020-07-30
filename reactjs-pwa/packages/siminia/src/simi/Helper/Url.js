cab2d08f94b7c7c7dcf2dcd3e92cb5150b1ab380
Cody Nguyen <cody@simicart.com>
2020-07-30 10:11:13 +0700

    Merge branch 'customize/2980-basmajiramez@gmail.com-Bianca-Nera' of https://github.com/Simicart/siminia into customize/2980-basmajiramez@gmail.com-Bianca-Nera, mergecart

--- reactjs-pwa/packages/siminia/src/simi/Helper/Url.js
+++ reactjs-pwa/packages/siminia/src/simi/Helper/Url.js
@@ -39,7 +39,7 @@ export const logoUrl = () => {
     }
     return window.SMCONFIGS.logo_url ?
         window.SMCONFIGS.logo_url :
-    'https://www.simicart.com/skin/frontend/default/simicart2.1/images/simicart/new_logo_small.png'
+    '/images/logo_footer.png'
 }
 
 /*
