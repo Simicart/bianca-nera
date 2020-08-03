b03d73a3bd97dbc9fea688dea55bc5f4ac795659
Robert <mrthinlt@gmail.com>
2020-08-03 10:06:26 +0700

    Fix bug home page no product right content, Fix bug missing designer name product detail

--- reactjs-pwa/packages/siminia/src/simi/App/Bianca/Home/HomeSection.js
+++ reactjs-pwa/packages/siminia/src/simi/App/Bianca/Home/HomeSection.js
@@ -211,7 +211,7 @@ const HomeSection = props => {
                 );
             }
 
-            if (productItems) {
+            if (productItems && productItems.length) {
                 return (
                     <div className="right-sec">
                         {
