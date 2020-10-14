7bb5451544c58c4adc33a30316e7deb3f076014a
Cody Nguyen <cody@simicart.com>
2020-10-14 12:10:36 +0700

    use one config, load config before client.js, fastly's back

--- reactjs-pwa/packages/siminia/src/simi/Config/index.js
+++ reactjs-pwa/packages/siminia/src/simi/Config/index.js
@@ -1,8 +1,29 @@
-import Identify from 'src/simi/Helper/Identify'
 
-// const simicartConfig = Identify.getAppDashboardConfigs();
-const simicartConfig = null;
-const config = simicartConfig ? simicartConfig['app-configs'][0] : null;
+window.DEFAULT_COLORS = {
+    key_color: '#ff9800',
+    top_menu_icon_color: '#ffffff',
+    button_background: '#101820',
+    button_text_color: '#ffffff',
+    menu_background: '#1b1b1b',
+    menu_text_color: '#ffffff',
+    menu_line_color: '#292929',
+    menu_icon_color: '#ffffff',
+    search_box_background: '#f3f3f3',
+    search_text_color: '#7f7f7f',
+    app_background: '#ffffff',
+    content_color: '#131313',
+    image_border_color: '#f5f5f5',
+    line_color: '#e8e8e8',
+    price_color: '#ab452f',
+    special_price_color: '#ab452f',
+    icon_color: '#717171',
+    section_color: '#f8f8f9',
+    status_bar_background: '#ffffff',
+    status_bar_text: '#000000',
+    loading_color: '#000000',
+};
+
+const config = window.DASHBOARD_CONFIG['app-configs'][0];
 
 export const configColor = {
     key_color: config ? config['theme']['key_color'] : window.DEFAULT_COLORS.key_color,
@@ -26,6 +47,6 @@ export const configColor = {
     status_bar_background: config ? config['theme']['status_bar_background'] : window.DEFAULT_COLORS.status_bar_background,
     status_bar_text: config ? config['theme']['status_bar_text'] : window.DEFAULT_COLORS.status_bar_text,
     loading_color: config ? config['theme']['loading_color'] : window.DEFAULT_COLORS.loading_color,
-    splash_screen_color : window.DEFAULT_COLORS.splash_screen_color ? window.DEFAULT_COLORS.splash_screen_color : '#fff',
-    loading_splash_screen_color : window.DEFAULT_COLORS.loading_splash_screen_color ? window.DEFAULT_COLORS.loading_splash_screen_color : '#000',
+    splash_screen_color: '#fff',
+    loading_splash_screen_color: '#000',
 };
