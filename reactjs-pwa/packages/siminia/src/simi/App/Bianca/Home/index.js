1c1262e878598db14e2a76ea502428833d31a6c7
Cody Nguyen <cody@simicart.com>
2020-10-14 08:26:53 +0700

    home lazyload content, Update currency title by locale

--- reactjs-pwa/packages/siminia/src/simi/App/Bianca/Home/index.js
+++ reactjs-pwa/packages/siminia/src/simi/App/Bianca/Home/index.js
@@ -4,7 +4,7 @@ import Banner from './Banner';
 import HomeCat from "./HomeCat";
 // import Brands from "./Brands";
 import HomeSection from "./HomeSection";
-// import LoadingSpiner from 'src/simi/BaseComponents/Loading/LoadingSpiner'
+import LoadingSpiner from 'src/simi/BaseComponents/Loading/LoadingSpiner'
 import { withRouter } from 'react-router-dom';
 // import ProductList from './ProductList';
 import Identify from 'src/simi/Helper/Identify';
@@ -15,39 +15,40 @@ import Designers from './Designers';
 import Instagram from './Instagram';
 import TitleHelper from 'src/simi/Helper/TitleHelper';
 // import Chats from 'src/simi/App/Bianca/BaseComponents/Chats';
+import LazyLoad from 'react-lazyload';
 require('./home.scss');
 
 if (getOS() === 'MacOS') require('./home-ios.scss');
 
 const Home = props => {
-    const {history} = props;
+    const { history } = props;
     const [isPhone, setIsPhone] = useState(window.innerWidth < 1024)
     // const simiSessId = Identify.getDataFromStoreage(Identify.LOCAL_STOREAGE, Constants.SIMI_SESS_ID)
     const cached_home = Identify.ApiDataStorage(`home_lite`)
     const storeConfig = Identify.getStoreConfig() || {};
     const config = storeConfig.simiStoreConfig && storeConfig.simiStoreConfig.config || {};
-    const {brands, instagram, seo} = config || [];
+    const { brands, instagram, seo } = config || [];
     const [data, setHomeData] = useState(cached_home);
 
     const resizePhone = () => {
         window.onresize = function () {
             const width = window.innerWidth;
             const newIsPhone = width < 1024
-            if(isPhone !== newIsPhone){
+            if (isPhone !== newIsPhone) {
                 setIsPhone(newIsPhone)
             }
         }
     }
     useEffect(() => {
-        if(!data) {
+        if (!data) {
             getHomeData(setData);
         }
         resizePhone();
-    },[data, isPhone])
+    }, [data, isPhone])
 
     const setData = (data) => {
-        if(!data.errors) {
-            Identify.ApiDataStorage(`home_lite`,'update', data);
+        if (!data.errors) {
+            Identify.ApiDataStorage(`home_lite`, 'update', data);
             setHomeData(data);
         }
     }
@@ -57,14 +58,14 @@ const Home = props => {
     // } 
 
     const home_meta = seo && seo.home_meta && seo.home_meta || null;
-    
+
     return (
         <div className={`home-wrapper ${getOS()}`}>
             {TitleHelper.renderMetaHeader({
                 title: Identify.__(home_meta && home_meta.title || 'Bianca Nera'),
                 desc: Identify.__(home_meta && home_meta.desc || '')
             })}
-            <div className={`banner-wrap ${isPhone ? 'mobile':''}`}>
+            <div className={`banner-wrap ${isPhone ? 'mobile' : ''}`}>
                 <Banner data={data} history={history} isPhone={isPhone} />
             </div>
 
@@ -84,14 +85,18 @@ const Home = props => {
             {/* <div className={`new-collections-wrap ${isPhone ? 'mobile':''}`}>
                 <Newcollections data={data} history={history} isPhone={isPhone}/>
             </div> */}
-            <div className={`shop-by-designers-wrap ${isPhone ? 'mobile':''}`}>
-                <Designers history={history} isPhone={isPhone}/>
-            </div>
-            {
-                instagram && instagram.enabled === '1' && instagram.userid &&
-                <div className={`shop-our-instagram-wrap ${isPhone ? 'mobile':''}`}>
-                    <Instagram data={instagram.userid} history={history} isPhone={isPhone}/>
+            <LazyLoad placeholder={<LoadingSpiner />}>
+                <div className={`shop-by-designers-wrap ${isPhone ? 'mobile' : ''}`}>
+                    <Designers history={history} isPhone={isPhone} />
                 </div>
+            </LazyLoad>
+            {
+                (instagram && instagram.enabled === '1' && instagram.userid) ?
+                    <LazyLoad placeholder={<LoadingSpiner />}>
+                        <div className={`shop-our-instagram-wrap ${isPhone ? 'mobile' : ''}`}>
+                            <Instagram data={instagram.userid} history={history} isPhone={isPhone} />
+                        </div>
+                    </LazyLoad> : ''
             }
             {/* <div className={`home-chats ${isPhone ? 'mobile':''}`}>
                 <Chats data={instant_contact} history={history} isPhone={isPhone}/>
